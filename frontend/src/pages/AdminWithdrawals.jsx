1a1e518572e67ca14c775fc670ded3205d33f2fe
satyam07july-png <https://github.com/satyam07july-png>
2026-08-05 12:27:39 +0530

    inital chnages

--- frontend/src/pages/AdminWithdrawals.jsx
+++ frontend/src/pages/AdminWithdrawals.jsx
@@ -124,6 +124,8 @@ export const AdminWithdrawals = () => {
           if (window.Razorpay) return resolve(true);
           const script = document.createElement('script');
           script.src = 'https://checkout.razorpay.com/v1/checkout.js';
+          script.crossOrigin = 'anonymous';
+          script.integrity = 'sha384-PbJqxqn7GZcnYbsMDvfE6NSEuLqoC0pYWRxqFfeKFJ2LaeFJiJHCDXienGv6N9WA';
           script.onload = () => resolve(true);
           script.onerror = () => resolve(false);
           document.body.appendChild(script);
